b82faa39afc16b1ba336c94ca5648a71ab091712
diabolique79 <m.mielnicka79@gmail.com>
2018-08-25 16:00:53 +0200

    11.1 - rozwiązanie zadania

--- src/Main.java
+++ src/Main.java
@@ -1,54 +1,109 @@
-import javax.sound.midi.Soundbank;
-import java.lang.reflect.Array;
+
 import java.util.ArrayList;
-import java.util.Collections;
+
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        ArrayList <Integer> list = new ArrayList<>();
+        ArrayList<Integer> list = new ArrayList<>();
 
-        Scanner scanner = new Scanner(System.in);
-        Integer liczba = 0;
-        int liczba1 = Integer.valueOf(liczba);
-//        System.out.println("Wpisz liczbę parzystą: ");
-//        liczba = scanner.nextInt();
-
-        while ((liczba%2==0)){
-
-            System.out.println("Wpisz liczbę parzystą: ");
-            liczba = scanner.nextInt();
-            list.add(liczba);
-        }
-        if (liczba%2==1){
-
-            list.remove(list.size()-1);
-            System.out.println(list);
-        }
-
-        int sum=0;
-        int i = 0;
-        for (i = 0; i < list.size(); i++) {
-
-            sum = sum + list.get(i);
-
-        }
-
-        System.out.println(" = " + sum);
-
-
-        //wyświetlanie odwrotne tablicy
-        for (int j = list.size() -1; j >= 0; j--) {
-
-            System.out.println(list.get(j));
-        }
-        Integer max = Collections.max(list);
-        System.out.println("Wartość maksymalna to: " + max);
-        Integer min = Collections.min(list);
-        System.out.println("Wartość minimalna to: " + min);
+        System.out.println("Program do obliczania sumy, maksymalnej i minamalnej wartości wprowadzonych przez użytkownika liczb dodatnich. Program zakończy się po wpisaniu pierwszej ujemnej wartości.");
+        readNumbers(list);
+        sumNumbers(list);
+        printReverse(list);
+        max(list);
+        min(list);
 
 
     }
+
+    public static void readNumbers(ArrayList list) {
+
+        Scanner scanner = new Scanner(System.in);
+        Integer liczba = 0;
+
+        while ((liczba >= 0)) {
+
+            System.out.println("Wpisz liczbę dodatnią: ");
+            liczba = scanner.nextInt();
+            list.add(liczba);
+        }
+        if (liczba < 0) {
+
+            list.remove(list.size() - 1);
+            System.out.println(list);
+        }
+    }
+
+    public static void sumNumbers(ArrayList list) {
+
+        System.out.println("Suma wartości w tablicy: ");
+        int sum = 0;
+
+        for (int i = 0; i < list.size(); i++) {
+            list.get(i);
+            sum = sum + ((Integer) list.get(i));
+            Integer z = list.size() - 1;
+            if(i == z) {
+                System.out.print(list.get(i) + " = ");
+            }else System.out.print(list.get(i) + " + ");
+
+        }
+
+        System.out.println(sum);
+
+    }
+
+    public static void printReverse(ArrayList list) {
+
+        System.out.println("Tablica w odwrotnej kolejności: ");
+        for (int j = list.size() - 1; j >= 0; j--) {
+
+            System.out.print(list.get(j) + " ");
+
+        }
+        System.out.println(" ");
+    }
+
+    public static void max(ArrayList list) {
+
+        int max = (Integer) list.get(0);
+
+        for (int i = 0; i < list.size(); i++) {
+
+            if (max < (Integer) list.get(i)) {
+
+                max = (Integer) list.get(i);
+            }
+
+        }
+        System.out.println("Wartość maksymalna to: " + max);
+
+    }
+
+    public static void min(ArrayList list) {
+
+        int min = (Integer) list.get(0);
+
+        for (int i = 0; i < list.size(); i++) {
+
+            if (min > (Integer) list.get(i)) {
+
+                min = (Integer) list.get(i);
+            }
+
+        }
+        System.out.println("Wartość minimalna to: " + min);
+
+    }
+
+
 }
+
+
+
+
+
+
